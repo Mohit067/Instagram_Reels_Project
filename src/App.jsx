5ba8a1162b380b0ea5cd4f4c713d2e049548ff7b
Mohit sahu <mohitsahu60067@gmail.com>
2025-03-01 15:01:07 +0530

    Fix: Integrated get all video api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { getAllVideos } from './apis/videos'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    getAllVideos();
+  }, [])
   return (
     <>
       <div>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
